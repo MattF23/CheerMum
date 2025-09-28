aa5f869d550d1373760cb7eabf4abb3f95ddf993
Matt <mattf789@gmail.com>
2025-09-28 11:00:30 +1000

    Added structure to suggestion and wifi activities. Set up test for switch.

--- app/src/main/java/com/example/cheermum/SuggestionConfigActivity.kt
+++ app/src/main/java/com/example/cheermum/SuggestionConfigActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.google.android.material.materialswitch.MaterialSwitch
+import android.widget.Toast
 
 
 class SuggestionConfigActivity : ComponentActivity(){
+    var suggestYogaValue: Boolean = true
+    var suggestOutsideValue: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,6 +24,11 @@ class SuggestionConfigActivity : ComponentActivity(){
         //Go home button
         findViewById<Button>(R.id.Go_home).setOnClickListener {
             finish()
+        }
+        //switches
+        findViewById<MaterialSwitch>(R.id.switch_yoga).setOnCheckedChangeListener { _, isChecked ->
+            suggestYogaValue = isChecked
+            Toast.makeText(this, suggestYogaValue.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 }
