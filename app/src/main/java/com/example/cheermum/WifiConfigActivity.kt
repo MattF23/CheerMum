a09ca2f7aec85aed062e887ea9c074431fdbb03a
Matt <mattf789@gmail.com>
2025-09-30 22:59:11 +1000

    Saves wifi uuid and password to dictionary. Started adding permissions to manifest.xml but doesn't work yet.

--- app/src/main/java/com/example/cheermum/WifiConfigActivity.kt
+++ app/src/main/java/com/example/cheermum/WifiConfigActivity.kt
@@ -2,6 +2,7 @@ package com.example.cheermum
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
@@ -15,6 +16,15 @@ class WifiConfigActivity : ComponentActivity(){
     override fun onResume(){
         super.onResume()
         setContentView(R.layout.wifi_config)
+
+        //Information!
+        val uuid = findViewById<EditText>(R.id.WifiName).text
+        val password = findViewById<EditText>(R.id.WifiPassword).text
+
+        //send data button
+        findViewById<Button>(R.id.Submit_wifi).setOnClickListener {
+            val wifiInfo = mapOf("uuid" to uuid, "password" to password)//Send this to pi somehow
+        }
 
         //Go home button
         findViewById<Button>(R.id.Go_home).setOnClickListener {
