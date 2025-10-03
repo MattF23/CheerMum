d67cc221815ed52d02176f1d4f715ecfabbb0e67
matt01 <matthewf789@gmail.com>
2025-10-03 20:02:22 +1000

    Can save and read. Running into crashes. May need to work on stream lining things.

--- app/src/main/java/com/example/cheermum/SuggestionConfigActivity.kt
+++ app/src/main/java/com/example/cheermum/SuggestionConfigActivity.kt
@@ -1,7 +1,7 @@
 package com.example.cheermum
 
 import android.os.Bundle
-import android.os.Environment
+//import android.os.Environment
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
@@ -9,10 +9,10 @@ import com.google.android.material.materialswitch.MaterialSwitch
 import android.widget.Toast
 import android.widget.EditText
 import java.io.File
+import org.json.JSONObject
 
 
 class SuggestionConfigActivity : ComponentActivity(){
-
     //Information
     var suggestYogaValue: Boolean = true
     var suggestOutsideValue: Boolean = true
@@ -27,21 +27,31 @@ class SuggestionConfigActivity : ComponentActivity(){
         setContentView(R.layout.suggestion_config)
 
         //More Information
-        val state: String? = Environment.getExternalStorageState()
+        //val state: String? = Environment.getExternalStorageState()
         val yogaMessage: EditText = findViewById(R.id.yoga_message)
         val outsideMessage: EditText = findViewById(R.id.outside_message)
-        var settingsSwitches = mapOf("yoga_suggestions" to true, "outside_suggestions" to true).toMutableMap()
-        var settingsText = mapOf("yoga_message" to yogaMessage.text.toString(), "outside_message" to outsideMessage.text.toString()).toMutableMap()
+        val settingsSwitches = mapOf("yoga_suggestion" to true, "outside_suggestion" to true).toMutableMap()
+        val settingsText = mapOf("yoga_message" to yogaMessage.text.toString(), "outside_message" to outsideMessage.text.toString()).toMutableMap()
 
         //Programmatically set values of switches and text when app is first launched
-        read()
+        val file = File(filesDir, "settingsSwitches.json")
+        if(!file.exists()){
+            save(settingsSwitches, settingsText, yogaMessage.text.toString(), outsideMessage.text.toString())
+        }
+        val settingsSwitchesTemp = read(file)
 
         //switches
-        findViewById<MaterialSwitch>(R.id.switch_yoga).setOnCheckedChangeListener { _, isChecked ->
+        val yogaSwitch = findViewById<MaterialSwitch>(R.id.switch_yoga)
+        val outsideSwitch = findViewById<MaterialSwitch>(R.id.switch_outside)
+        yogaSwitch.isChecked = settingsSwitchesTemp.getBoolean("yoga_suggestion")
+        outsideSwitch.isChecked = settingsSwitchesTemp.getBoolean("outside_suggestion")
+
+
+        yogaSwitch.setOnCheckedChangeListener { _, isChecked ->
             suggestYogaValue = isChecked
             Toast.makeText(this, suggestYogaValue.toString(), Toast.LENGTH_SHORT).show()
         }
-        findViewById<MaterialSwitch>(R.id.switch_outside).setOnCheckedChangeListener { _, isChecked ->
+        outsideSwitch.setOnCheckedChangeListener { _, isChecked ->
             suggestOutsideValue = isChecked
             Toast.makeText(this, suggestOutsideValue.toString(), Toast.LENGTH_SHORT).show()
         }
@@ -57,8 +67,8 @@ class SuggestionConfigActivity : ComponentActivity(){
         }
     }
     fun save(settingsSwitches: MutableMap<String, Boolean>, settingsText: MutableMap<String, String>, yogaMessage: String, outsideMessage: String){
-        settingsSwitches.put("yoga_suggestions", suggestYogaValue)
-        settingsSwitches.put("outside_suggestions", suggestOutsideValue)
+        settingsSwitches.put("yoga_suggestion", suggestYogaValue)
+        settingsSwitches.put("outside_suggestion", suggestOutsideValue)
 
         settingsText.put("yoga_message", yogaMessage)
         settingsText.put("outside_message", outsideMessage)
@@ -66,8 +76,9 @@ class SuggestionConfigActivity : ComponentActivity(){
         val file = File(filesDir, "settingsSwitches.json")
         file.writeText(settingsSwitches.toString())
     }
-    fun read(){
-        val file = File(filesDir, "settingsSwitches.json")
+    fun read(file: File): JSONObject{
         Toast.makeText(this, file.inputStream().readBytes().toString(Charsets.UTF_8), Toast.LENGTH_LONG).show()
+
+        return JSONObject(file.inputStream().readBytes().toString(Charsets.UTF_8))
     }
 }
