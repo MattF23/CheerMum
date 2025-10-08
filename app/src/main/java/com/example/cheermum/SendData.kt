dd4f7d9b1c47de34a25b1bd11357e665aee3d670
matt01 <matthewf789@gmail.com>
2025-10-08 20:58:59 +1100

    Adding back but without my password

--- app/src/main/java/com/example/cheermum/SendData.kt
+++ app/src/main/java/com/example/cheermum/SendData.kt
@@ -1,29 +1,84 @@
 package com.example.cheermum
 
-import android.content.Context
 import android.util.Log
-
 import com.jcraft.jsch.ChannelExec
 import com.jcraft.jsch.JSch
-import com.jcraft.jsch.Session
+import kotlinx.coroutines.Runnable
 import java.io.File
+import kotlin.concurrent.thread
 
-fun sendData(applicationContext: Context, message: String, file: File): String {
-    val jsch = JSch()
-    val session = jsch.getSession("admin", "PUT PI IP HERE", 22)
-    session.setPassword("admin")
-    session.setConfig("StrictHostKeyChecking", "no")
-    session.connect()
+fun sendData(file: File): String {
+    val output = StringBuilder()
+    thread {
+        try {
+            val jsch = JSch()
+            val session = jsch.getSession("matt", "localhost", 22)
+            session.setPassword("Your password here")
+            session.setConfig("StrictHostKeyChecking", "no")
+            session.connect()
 
-    if(session.isConnected){
-        Log.i("Connection", "Is connected!")
+            if (session.isConnected) {
+                Log.i("Connection", "Is connected!")
+            }
+
+            val command = "scp -p -t $file"
+
+            val channel = session.openChannel("exec") as ChannelExec
+            channel.setCommand(command)
+            channel.connect()
+
+            val inputStream = channel.inputStream
+            val buffer = ByteArray(1024)
+            var bytesRead: Int
+
+            while (inputStream.read(buffer).also { bytesRead = it } > 0) {
+                output.append(String(buffer, 0, bytesRead))
+            }
+            channel.disconnect()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
-
-    val command = "scp -p -t " + file
-
-    val channel = session.openChannel("exec")
-    //Then need to set the command of the channel somehow
-    //Not done but have to go!
-
-    return "Successfully sent data"
+    var result = ""
+    if (output.toString() == ""){
+        result = "Something went wrong"
+    }
+    result = result + output.toString()//Returns output if it exists. Error message otherwise
+    return result
 }
+
+/*class DataSender(): Runnable{
+    override fun run(){
+        try {
+        val jsch = JSch()
+        val session = jsch.getSession("matt", "localhost", 22)
+        session.setPassword("mckinnon24680")
+        session.setConfig("StrictHostKeyChecking", "no")
+        session.connect()
+
+        if (session.isConnected) {
+            Log.i("Connection", "Is connected!")
+        }
+
+        val command = "scp -p -t $file"
+
+        val channel = session.openChannel("exec") as ChannelExec
+        channel.setCommand(command)
+        channel.connect()
+
+        val inputStream = channel.inputStream
+        val buffer = ByteArray(1024)
+        var bytesRead: Int
+        val output = StringBuilder()
+
+        while (inputStream.read(buffer).also { bytesRead = it } > 0) {
+            output.append(String(buffer, 0, bytesRead))
+        }
+        channel.disconnect()
+
+
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+}*/
