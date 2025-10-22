bb8de355736b9dba8c5183931b3c02474baab202
matt01 <matthewf789@gmail.com>
2025-10-22 17:23:24 +1100

    Removed redundant/deprecated settings files. Removed mqtt libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,6 @@ android {
 }
 
 dependencies {
-    //implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.4")
-    //implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
-    //implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
-
     implementation("com.github.mwiede:jsch:0.2.11")
 
     implementation(libs.androidx.core.ktx)
